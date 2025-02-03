320d7bad79d33f6434de7f16cfe1a00b985428b3
Muhammad Sadam Muneer <sadam.fullstackdev@gmail.com>
2025-02-03 21:52:42 +0500

    order in insert operator

---
+++ 03-CRUD/04-insertOrder.js
@@ -0,0 +1,51 @@
+db.users.insertMany([
+  {
+    _id: 1,
+    name: "BookA",
+    price: 1,
+  },
+  {
+    _id: 2,
+    name: "BookB",
+    price: 2,
+  },
+  {
+    _id: 3,
+    name: "BookC",
+    price: 3,
+  },
+]);
+
+// now if i insert other three books data with same _id , then it will
+// show error and dont insert all data next to that matched _id
+// so in real , it should only show error on that same existed _id and should store other data
+// so for that purpose , we will insert its order
+
+//insert havae two thngs
+//  =>  insertMany(data , options)
+// so add options
+
+db.users.insertMany(
+  [
+    {
+      _id: 3,
+      name: "Book3",
+      price: 1,
+    },
+    {
+      _id: 4,
+      name: "BookD",
+      price: 2,
+    },
+    {
+      _id: 5,
+      name: "BookE",
+      price: 3,
+    },
+  ],
+  { ordered: false }
+);
+
+// by-default { ordered: true }
+// Now this will not save _id : 3 data , due to same key
+// but will save all other data instead of matched _id

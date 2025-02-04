9af373fcfffa9075be27a08f32b397e1b5a96bf2
Muhammad Sadam Muneer <sadam.fullstackdev@gmail.com>
2025-02-04 21:57:59 +0500

    Validations in mongoDB

--- 04-Validations/validation.js
+++ 04-Validations/validation.js
@@ -22,4 +22,34 @@ db.createCollection("users", {
       },
     },
   },
+  validationAction: "error", // can also be warn
+});
+
+
+// modify
+
+db.runCommand({
+  collMod: "users",
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      required: ["name", "age", "dept"],
+      properties: {
+        name: {
+          bsonType: "string",
+          description: "must be a string and is required",
+        },
+        age: {
+          bsonType: "int",
+          minimum: 18,
+          description: "must be an integer and at least 18",
+        },
+        dept: {
+          bsonType: "string",
+          description: "must be a string and is required",
+        },
+      },
+    },
+  },
+  validationAction: "error", // can also be warn
 });

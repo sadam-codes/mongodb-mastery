7cda6a6eac4d8a7643f6fa9d5c1a366e972b58cd
Muhammad Sadam Muneer <sadam.fullstackdev@gmail.com>
2025-02-06 00:45:49 +0500

    logical operators

--- 07-Operators/LogicalOperators.js
+++ 07-Operators/LogicalOperators.js
@@ -6,7 +6,7 @@ db.users.find();
 
 // Logical operators
 
-db.users.find({ $or: [{ age: { $lte: 25 } }, { age: { $gte: 20 } }] });
+db.users.find({ $or: [{ name: { $eq: "Ali" } }, { age: { $eq: 25 } }] });
 db.users.find({ $and: [{ name: { $eq: "Ali" } }, { age: { $eq: 25 } }] });
 db.users.find({ $nor: [{ name: { $eq: "Hamza" } }, { age: { $eq: 20 } }] });
 

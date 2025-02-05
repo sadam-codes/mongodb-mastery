3165fc6cf37deb997cfbd16df9076c219aae3c8f
Muhammad Sadam Muneer <sadam.fullstackdev@gmail.com>
2025-02-05 13:41:46 +0500

    feat: Import student data into MongoDB
    Added a JSON file containing student details and imported it into the MongoDB 'university' database.

---
+++ 06-MongoImport/import_JSON.js
@@ -0,0 +1,52 @@
+// download =>  MongoDB Command Line Database Tools Download (msi)
+// install
+// copy path of tools till bin and set .env
+//
+// now you can run the mongoimport command in cmd
+// dummy Data
+
+//steps
+// 1- Save the JSON data as students.json
+
+[
+  {
+    name: "Ali Khan",
+    department: "Computer Science",
+    section: "A",
+  },
+  {
+    name: "Ahmad",
+    department: "Software Engineering",
+    section: "B",
+  },
+  {
+    name: "Bilal Hussain",
+    department: "Information Technology",
+    section: "A",
+  },
+  {
+    name: "Sara Malik",
+    department: "Electrical Engineering",
+    section: "C",
+  },
+  {
+    name: "Hamza Raza",
+    department: "Mechanical Engineering",
+    section: "B",
+  },
+];
+
+// 2- Run the mongoimport Command in new cmd
+// mongoimport --file "C:\Users\k\Desktop\hy\dummy.json" --db university --collection students --jsonArray --drop
+
+// explanation
+
+// --file → Specifies the JSON file path.
+// --db university → Uses the university database.
+// --collection students → Imports data into the students collection.
+// --jsonArray → Specifies that the file contains an array of JSON objects.
+// --drop → Deletes existing data in the collection before importing.
+
+// verify
+// use university
+// db.students.find()

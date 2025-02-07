de6622d221670da2cb33b2b4b9c1881465ddd57d
Muhammad Sadam Muneer <sadam.fullstackdev@gmail.com>
2025-02-07 21:58:43 +0500

    feat : Apply queries on Arrays

--- 09-QueryOnArray/Array.js
+++ 09-QueryOnArray/Array.js
@@ -18,12 +18,15 @@ db.users.insertOne({
     },
   ],
 });
+//--------------------------------
 
 // find students whos hobbies is cricket
 db.users.find({ Hobbies: "Cicket" });
 
 // students who have experience in amazon
 db.users.find({ "experience.company": "Amazon" });
+
+//-------------------------------
 
 // How many students have worked in Xeven
 db.users.find({ "experience.company": "Xeven" }).size();
@@ -35,8 +38,7 @@ db.users.find({ experience: { $size: 3 } });
 // $size operator does not support comparison operators like $lte or $gte e.t.c
 db.users.find({ experience: { $size: { $lte: 3 } } }); // this will show error
 // show same thing with different method
-db.users.find({ $expr: { $lte: [{ $size: "$experience" }, 3]}}); // The argument to $size must be an array, but was of type: missing
-
+db.users.find({ $expr: { $lte: [{ $size: "$experience" }, 3] } }); // The argument to $size must be an array, but was of type: missing
 
 // perfect (first check it exists and then apply condition)
 db.users.find({
@@ -44,4 +46,26 @@ db.users.find({
     { experience: { $exists: true } },
     { $expr: { $lte: [{ $size: "$experience" }, 3] } },
   ],
+});
+
+//---------------------------------
+
+// How many students whose Hobbies are walking and Reading ?
+
+// but this will exactly match same pattern
+db.users.find({ Hobbies: ["Reading", "Walking"] }).count();
+
+// so we use this method ($all)
+db.users.find({ Hobbies: { $all: ["Walking", "Reading"] } }).count();
+
+// Following tells that , in walking or reading ,if anyone has been stisfied and show data
+db.users.find({ Hobbies: { $in: ["Walking", "Reading"] } }).count();
+
+//---------------------------------
+
+// $elemMatch
+db.users.find({ experience: { $elemMatch: { company: "Xeven" } } });
+// another example
+db.users.find({
+  experience: { $elemMatch: { company: "Xeven", duration: { $gte: 1 } } },
 });

9449551d497d7826df336327ee6b00fd17d9d937
Muhammad Sadam Muneer <sadam.fullstackdev@gmail.com>
2025-02-25 22:55:10 +0500

    aggregations in mongodb

---
+++ 11-Aggregation/Aggregation.js
@@ -0,0 +1,5 @@
+// MongoDB Aggregation Pipeline -: 
+
+// The Aggregation Pipeline in MongoDB is like an assembly line for your data. 
+// It processes documents in stages, where each stage transforms the data before
+// passing it to the next stage.
